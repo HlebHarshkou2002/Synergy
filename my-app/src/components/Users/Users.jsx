d78a9117119c4a35d88259dd4e83ea69848cf262
HlebHarshkou2002 <erichkrause2012@tut.by>
2023-02-15 15:25:19 +0300

    refactored a users page container component

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -1,101 +1,73 @@
 import React from "react";
 import User from "./user/User";
-import axios from "axios";
 import styles from "./Users.module.css";
 
-class Users extends React.Component {
-  componentDidMount() {
-    if (this.props.usersData.length === 0) {
-      axios
-        .get(
-          `https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`
-        )
-        .then((response) => {
-          console.log("Response: ", response);
-          this.props.setUsers(response.data.items);
-          this.props.setTotalUsersCount(response.data.totalCount);
-        });
-    }
+const Users = (props) => {
+
+  let usersElements = props.usersData.map((u) => (
+    <User
+      name={u.name}
+      followed={u.followed}
+      id={u.id}
+      // surname={u.surname}
+      photoUrl={
+        u.photos.small === null
+          ? "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+          : u.photos.small
+      }
+      status={u.status}
+      // country={u.location.country}
+      // city={u.location.city}
+
+      follow={props.follow} // из контейнерной компоненты метод передаётся презентационной в пропсах
+      unFollow={props.unFollow}
+      active={u.active}
+    />
+  ));
+
+  let pagesCount = Math.ceil(
+    props.totalUsersCount / props.pageSize
+  );
+
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        console.log("Response: ", response);
-        this.props.setUsers(response.data.items);
-      });
-  };
+  let slicedPages;
+  let curPage = props.currentPage;
+  if (curPage - 5 < 0) {
+    slicedPages = pages.slice(0, 5);
+  } else {
+    slicedPages = pages.slice(curPage - 3, curPage + 2);
+  }
 
-  render() {
-    let usersElements = this.props.usersData.map((u) => (
-      <User
-        name={u.name}
-        followed={u.followed}
-        id={u.id}
-        // surname={u.surname}
-        photoUrl={
-          u.photos.small === null
-            ? "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
-            : u.photos.small
-        }
-        status={u.status}
-        // country={u.location.country}
-        // city={u.location.city}
+  return (
+    <div>
+      <h1>USERS</h1>
 
-        follow={this.props.follow} // из контейнерной компоненты метод передаётся презентационной в пропсах
-        unFollow={this.props.unFollow}
-        active={u.active}
-      />
-    ));
-
-    let pagesCount = Math.ceil(
-      this.props.totalUsersCount / this.props.pageSize
-    );
-
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-
-    let slicedPages;
-    let curPage = this.props.currentPage;
-    if (curPage - 5 < 0) {
-      slicedPages = pages.slice(0, 5);
-    } else {
-      slicedPages = pages.slice(curPage - 3, curPage + 2);
-    }
-
-    return (
-      <div>
-        <h1>USERS</h1>
-
-        <div className={styles.pagesBar}>
-          {slicedPages.map((p) => {
-            return (
-              <div
-                className={
-                  this.props.currentPage === p
-                    ? styles.selectedPage
-                    : styles.pageItem
-                }
-                onClick={(e) => {
-                  this.onPageChanged(p);
-                }}
-              >
-                {p}
-              </div>
-            );
-          })}
-        </div>
-
-        {usersElements}
+      <div className={styles.pagesBar}>
+        {slicedPages.map((p) => {
+          return (
+            <div
+              className={
+                props.currentPage === p
+                  ? styles.selectedPage
+                  : styles.pageItem
+              }
+              onClick={(e) => {
+                props.onPageChanged(p);
+              }}
+            >
+              {p}
+            </div>
+          );
+        })}
       </div>
-    );
-  }
-}
+
+      {usersElements}
+    </div>
+  );
+};
 
 export default Users;
--- my-app/src/components/Users/UsersContainer.jsx
+++ my-app/src/components/Users/UsersContainer.jsx
@@ -1,37 +1,88 @@
 import React from "react";
 import { connect } from "react-redux";
-import { followActionCreator, unFollowActionCreactor, setUsersActionCreator, setCurrentPageActionCreator, setTotalUsersCountActionCreator } from "../../redux/users-reducer";
+import {
+  followActionCreator,
+  unFollowActionCreactor,
+  setUsersActionCreator,
+  setCurrentPageActionCreator,
+  setTotalUsersCountActionCreator,
+} from "../../redux/users-reducer";
 import Users from "./Users";
+import axios from "axios";
 
-const mapStateToProps = (state) => {
-    return {
-        usersData: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
+class UsersAPIContainer extends React.Component {
+  componentDidMount() {
+    if (this.props.usersData.length === 0) {
+      axios
+        .get(
+          `https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`
+        )
+        .then((response) => {
+          console.log("Response: ", response);
+          this.props.setUsers(response.data.items);
+          this.props.setTotalUsersCount(response.data.totalCount);
+        });
     }
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        console.log("Response: ", response);
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+    <Users 
+    usersData={this.props.usersData} 
+    follow={this.props.follow}
+    unFollow={this.props.unFollow}
+    totalUsersCount={this.props.totalUsersCount}
+    pageSize={this.props.pageSize}
+    currentPage={this.props.currentPage}
+    onPageChanged={this.onPageChanged}
+    />);
+  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followActionCreator(userId));
-        },
-        unFollow: (userId) => {
-            dispatch(unFollowActionCreactor(userId));
-        },
-        setUsers: (users) => {
-            dispatch(setUsersActionCreator(users))
-        },
-        setCurrentPage: (pageNumber) => {
-            dispatch(setCurrentPageActionCreator(pageNumber));
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(setTotalUsersCountActionCreator(totalCount));
-        }
-    }
-} 
+const mapStateToProps = (state) => {
+  return {
+    usersData: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
+  };
+};
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    follow: (userId) => {
+      dispatch(followActionCreator(userId));
+    },
+    unFollow: (userId) => {
+      dispatch(unFollowActionCreactor(userId));
+    },
+    setUsers: (users) => {
+      dispatch(setUsersActionCreator(users));
+    },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPageActionCreator(pageNumber));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setTotalUsersCountActionCreator(totalCount));
+    },
+  };
+};
+
+const UsersContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UsersAPIContainer);
 
 export default UsersContainer;

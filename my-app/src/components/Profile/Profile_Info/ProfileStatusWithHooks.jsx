e4daef6d326e84e0f9904f3968b775e18fb894c0
HlebHarshkou2002 <erichkrause2012@tut.by>
2023-06-04 08:18:04 +0300

    added useEffect to profileStatus component

--- my-app/src/components/Profile/Profile_Info/ProfileStatusWithHooks.jsx
+++ my-app/src/components/Profile/Profile_Info/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     EditOutlined
   } from '@ant-design/icons';
@@ -7,6 +7,10 @@ import {
 const ProfileStatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true);
